dcda584cf866e6f2887cafa76c60d4d34b4a61b0
mkthakurTesting <murariraj.three@gmail.com>
2018-12-17 07:13:01 +0530

    grid handling

--- src/test/java/staticDataManager/ApplicationLevelMethod.java
+++ src/test/java/staticDataManager/ApplicationLevelMethod.java
@@ -1,58 +1,75 @@
 package staticDataManager;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
+import helper.Action;
 import uiAction.Element;
 
 public class ApplicationLevelMethod {
 
-	public static int getTotalSetOfData(WebDriver driver, By locator) {
+	private static Logger logger = Logger.getLogger(ApplicationLevelMethod.class);
 
-		int count = Element.elementCount(driver, locator);
-
-		int actualDataCount = count - 1;
-
-		return actualDataCount;
-
-	}
-
-	@Test
-	public static void getColumnHeading(WebDriver driver, String columnName) {
-
-		By columnLoc = By.xpath("//tr[1]/th");
+	public static boolean isColumnMatching(WebDriver driver, String columnName) {
+		boolean b = false;
+		By columnLoc = By.xpath("//th");
 		int columnCount = Element.elementCount(driver, columnLoc);
 
 		for (int i = 2; i <= columnCount; i++) {
 
-			String columnLocator = "//tr[1]/th[" + i + "]/a";
+			String dynamicColumnString = "(//th)[" + i + "]/a";
+			By dynamicColumnLoc = By.xpath(dynamicColumnString);
+			String columnActualText = Element.getText(driver, dynamicColumnLoc);
+			if (columnActualText.equalsIgnoreCase(columnName)) {
 
-			By columnHeardingLoc = By.xpath(columnLocator);
-
-			String getcolumnName = Element.getText(driver, columnHeardingLoc);
-
-			By rowLocator = By.xpath("//tr");
-			
-			int counttotalRow = Element.elementCount(driver, rowLocator);
-			
-			
-
-			if (getcolumnName.equalsIgnoreCase(columnName)) {
-                
-				 int   totalData = (counttotalRow -1) * 5 ;
-				  
-				  
-				 
-				  
+				b = true;
+				break;
 			}
 
 		}
+		return b;
+
+	}
+	public static int getIndexValueWhereColumnMatching(WebDriver driver, String columnName) {
+		int b = 0;
+		By columnLoc = By.xpath("//th");
+		int columnCount = Element.elementCount(driver, columnLoc);
+
+		for (int i = 2; i <= columnCount; i++) {
+
+			String dynamicColumnString = "(//th)[" + i + "]/a";
+			By dynamicColumnLoc = By.xpath(dynamicColumnString);
+			String columnActualText = Element.getText(driver, dynamicColumnLoc);
+			if (columnActualText.equalsIgnoreCase(columnName)) {
+             
+				b = i; 
+			
+				break;
+			}
+
+		}
+		return b;
 
 	}
 
-	public static void getGridSpaceficData(String columnName, By locator) {
+	public static int getTotalRecordsData(WebDriver driver) {
+		int recordData = 0;
 
+		By rowLoc = By.xpath("//tr");
+		int rowCount = Element.elementCount(driver, rowLoc) - 1;
+		if (rowCount >= 1) {
+
+			recordData = rowCount * 4;
+
+		} else {
+
+			logger.info("*************Record is not Available ****************");
+
+		}
+		logger.info("getTotalRecordsData()....Returning totalRecordData is " + recordData);
+		return recordData;
 	}
-
+   
 }
